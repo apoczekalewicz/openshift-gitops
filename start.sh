b6cf223f7bded1127b066028e89fac71b762dada
Artur <artur@poczekalewicz.net>
2023-05-18 11:40:56 +0200

    commit

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 oc apply -f bootstrap/gitops/gitops-operator.yaml
 read A
+echo Click Enter for approval install-plan
 PLAN=$(oc get installplan -A | grep openshift-gitops-operator | awk '{print $2}')
 oc patch installplan $PLAN --namespace openshift-operators --type merge --patch '{"spec":{"approved":true}}'
+echo Click Enter for bootstrap cluster 
 read A
 oc apply -k bootstrap/default/
 read A
